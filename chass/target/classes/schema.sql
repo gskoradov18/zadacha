755815605e5fb1bb6d110b8eca65f0eb548c0612
gskoradov18 <58163119+gskoradov18@users.noreply.github.com>
2023-01-10 15:37:31 +0200

    h

---
+++ chass/target/classes/schema.sql
@@ -0,0 +1,12 @@
+create table theRock(
+ id int auto_increment,
+ userName varchar(30) NOT NULL,
+ email varchar(35) NOT NULL,
+ description varchar(300)
+);
+
+INSERT INTO theRock(userName, email, description) Values ('Anton','hekimqn@gmail.com','Obicham da digam tejko kato skalata');
+
+INSERT INTO theRock(userName, email, description) Values ('Petar','petkanov@gmail.com','Ypollo');
+
+INSERT INTO theRock(userName, email, description) Values ('Ivan','Ivanov@gmail.com','Ivan e gei');
