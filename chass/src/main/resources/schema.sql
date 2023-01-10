6e9af525c666d7565161511eede00f32b68fb913
gskoradov18 <58163119+gskoradov18@users.noreply.github.com>
2023-01-10 15:36:56 +0200

    Delete chass directory

--- chass/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-create table theRock(
- id int auto_increment,
- userName varchar(30) NOT NULL,
- email varchar(35) NOT NULL,
- description varchar(300)
-);
-
-INSERT INTO theRock(userName, email, description) Values ('Anton','hekimqn@gmail.com','Obicham da digam tejko kato skalata');
